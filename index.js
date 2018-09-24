1b67ae57a84e01294da3e0ea192c72899ef57c67
acappello459 <acappello459@outlook.com>
2018-09-24 10:59:20 -0400

    add q 1 thru 3

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var box1 = document.getElementById('box1')
+var message1 = document.getElementById('message1')
 var box2 = document.getElementById('box2')
+var message2 = document.getElementById('message2')
 var box3 = document.getElementById('box3')
+var writer = document.getElementById('writer')
 
 // #1
 box1.addEventListener('click',function(){
-  box1.innerHTML = "I'm right!"
+  message1.innerHTML = "I'm right!"
 })
 box2.addEventListener('click',function(){
-  box2.innerHTML = "No, I'm right"
+  message2.innerHTML = "No, I'm right"
 })
 
 // #2
 box3.addEventListener('mouseover',function(){
-  box3.innerHTML = "Hey I said don't hover me"
+alert("I said don't hover over me!!")
+})
+
+document.addEventListener('keypress', function(e){
+  writer.innerHTML += e.key
 })
